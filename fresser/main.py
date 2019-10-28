14d9e004ffea99db7beedd99efafac566479b1b5
ponomarenkoaa <ponomarenkoaa25@gmail.com>
2019-10-28 21:54:09 +0100

    add trigger by new file creation

--- fresser/main.py
+++ fresser/main.py
@@ -1,14 +1,44 @@
+import argparse
 import logging
 import time
+from PyEIS import extract_mpt
+import os
+from watchdog.events import LoggingEventHandler
+from watchdog.observers import Observer
+from watchdog.events import LoggingFileSystemEventHandler
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 
-def action_recorder():
-    logging.info("application is running")
+class MprFilesHandler(LoggingFileSystemEventHandler):
+    def on_created(self, event):
+        if event.is_directory:
+            logging.info("Directory %s created" % event.src_path)
+        else:
+            logging.info("File %s created" % event.src_path)
+            if event.src_path.endswith(".mpt"):
+                logging.info("New file with MPT format: %s" % event.src_path)
+                if event.src_path.endswith("PEIS_C01.mpt"):
+                    file_path, file_name = os.path.split(event.src_path)
+                    data = extract_mpt(file_path + "/", file_name)
+                    print(data.head())
 
 
 if __name__ == '__main__':
-    while True:
-        action_recorder()
-        time.sleep(5)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=str)
+    args = parser.parse_args()
+
+    event_handler = MprFilesHandler()
+    observer = Observer()
+    observer.schedule(event_handler, args.path, recursive=True)
+    logging.info("Observing directory %s" % args.path)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
