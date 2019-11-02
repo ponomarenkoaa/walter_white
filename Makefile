ce1fdf1e2e2b6adf439261a4f72afe637ff818cb
ponomarenkoaa <ponomarenkoaa25@gmail.com>
2019-11-02 17:56:52 +0100

    add impedanz-logic and mp import

--- Makefile
+++ Makefile
@@ -2,3 +2,11 @@
 run-app:
 	docker build -t fresser .
 	docker run -it fresser
+
+run-app-local:
+	python fresser/main.py /Users/anna.ponomarenko/Work/walter_white_project/CLER_HCl
+
+run-trigger:
+	rm -rf /Users/anna.ponomarenko/Work/walter_white_project/CLER_HCl/trigger
+	cp -R /Users/anna.ponomarenko/Work/walter_white_project/trigger /Users/anna.ponomarenko/Work/walter_white_project/CLER_HCl
+
--- fresser/main.py
+++ fresser/main.py
@@ -2,8 +2,8 @@ import argparse
 import logging
 import os
 import time
-import pandas as pd
 
+import pandas as pd
 from PyEIS import extract_mpt
 from watchdog.events import LoggingFileSystemEventHandler
 from watchdog.observers import Observer
@@ -13,39 +13,69 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 
+def prepare_peis_c01_file(fpath: str) -> pd.DataFrame:
+    file_path, file_name = os.path.split(fpath)
+    peis1 = extract_mpt(file_path + "/", file_name)
+    print(peis1.head())
+    print(list(peis1.columns.values))
+    print(peis1[['Z_mag', 'Z_phase']])
+    peis1["Z_mag_shifted"] = peis1["Z_mag"].shift()
+    peis1["Z_phase_shifted"] = peis1["Z_phase"].shift()
+
+    def impedanzkorr(z_mag_shifted, z_phase_shifted, z_phase):
+        if (z_phase < z_phase_shifted) & (z_phase * z_phase_shifted < 0):
+            return z_mag_shifted
+        else:
+            return None
+
+    peis1["Impedanz1"] = peis1[["Z_mag_shifted", "Z_phase_shifted", "Z_phase"]].apply(
+        lambda imp_args: impedanzkorr(*imp_args), axis=1)
+    return peis1
+
+
+def prepare_mp_c01_file(fpath: str) -> pd.DataFrame:
+    file_path, file_name = os.path.split(fpath)
+    mp = extract_mpt(file_path + "/", file_name)
+    print(mp.head())
+    print(list(mp.columns.values))
+    mp["counter_inc_shifted"] = mp["counter inc."].shift()
+
+    def mpfun(counter_inc_shifted, counter_inc, ns_changes):
+        if (counter_inc * counter_inc_shifted == 0) & ((abs(counter_inc_shifted - counter_inc)) == 1):
+            return ns_changes
+        else:
+            return 0
+
+    mp["cycle"] = mp[["counter_inc_shifted", "counter inc.", "Ns_changes"]].apply(
+        lambda cycle_args: mpfun(*cycle_args), axis=1).cumsum()
+
+    return mp
+
+
 class MprFilesHandler(LoggingFileSystemEventHandler):
     def on_created(self, event):
         if event.is_directory:
             logging.info("Directory %s created" % event.src_path)
-        else:
-            logging.info("File %s created" % event.src_path)
-            if event.src_path.endswith(".mpt"):
-                logging.info("New file with MPT format: %s" % event.src_path)
-                if event.src_path.endswith("03_PEIS_C01.mpt"):
-                    file_path, file_name = os.path.split(event.src_path)
-                    peis1 = extract_mpt(file_path + "/", file_name)
-                    print(peis1.head())
-                    print(list(peis1.columns.values))
-                    print(peis1[['Z_mag', 'Z_phase']])
-                    peis1["Z_mag_shifted"] = peis1["Z_mag"].shift()
-                    peis1["Z_phase_shifted"] = peis1["Z_phase"].shift()
+            time.sleep(8)
+            directory_files = [os.path.join(event.src_path, f) for f in os.listdir(event.src_path)]
 
-                    def handler(Z_mag_shifted, Z_phase_shifted, Z_phase):
-                        if ((Z_phase < Z_phase_shifted) & (Z_phase * Z_phase_shifted < 0)):
-                            return Z_mag_shifted
-                        else:
-                            return None
+            peis_c01_files = [f for f in directory_files if f.endswith("03_PEIS_C01.mpt")]
+            mp_c01_files = [f for f in directory_files if f.endswith("04_MP_C01.mpt")]
 
-                    peis1["Impedanz1"] = peis1[["Z_mag_shifted", "Z_phase_shifted", "Z_phase"]].apply(lambda args: handler(*args), axis=1)
-                    print(peis1)
+            if len(peis_c01_files) != 1:
+                raise BaseException("Problem with peis c01 files, peis file list %s" % peis_c01_files)
 
-                if event.src_path.endswith("04_MP_C01.mpt"):
-                    file_path, file_name = os.path.split(event.src_path)
-                    mp = extract_mpt(file_path + "/", file_name)
-                    print(mp.head())
-                    print(list(mp.columns.values))
+            if len(mp_c01_files) != 1:
+                raise BaseException("Problem with mp c01 files")
 
+            peis_c01_df = prepare_peis_c01_file(peis_c01_files[0])
+            mp_c01_df = prepare_mp_c01_file(mp_c01_files[0])
 
+            peis1_value = peis_c01_df[pd.notnull(peis_c01_df["Impedanz1"])].iloc[0, :]["Impedanz1"]
+            print(peis1_value)
+
+            mp_c01_df["U korr/V"] = mp_c01_df["EweV"] - (mp_c01_df["I_avg"] / 1000 * peis1_value)
+            print(mp_c01_df)
 
 
 if __name__ == '__main__':
