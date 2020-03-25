3012800cd14ed78b0698f842dc26be9ebcf6285f
ponomarenkoaa <ponomarenkoaa25@gmail.com>
2020-03-25 21:47:37 +0100

    add new probe data sources

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 
 run-appliance:
-	docker-compose up --build
+	docker-compose up --build --remove-orphans -V
 
 trigger-test:
 	docker-compose exec mendeleev cp -R /usr/bin/app/tests/resources/trigger /usr/bin/app/data_input/
+
+connect-psql:
+	docker-compose  exec postgres bash -c 'psql -U mendeleev mendeleev'
--- mendeleev/main.py
+++ mendeleev/main.py
@@ -25,61 +25,116 @@ def impedanzkorr(z_mag_shifted, z_phase_shifted, z_phase):
         return None
 
 
-def extract_mpt_wrapper(fpath: str) -> pd.DataFrame:
-    file_path, file_name = os.path.split(fpath)
-    return extract_mpt(file_path + "/", file_name)
-
-
-def prepare_peis_file(fpath: str) -> pd.DataFrame:
-    peis = extract_mpt_wrapper(fpath)
+def prepare_peis_data(peis: pd.DataFrame) -> pd.DataFrame:
     peis["Z_mag_shifted"] = peis["Z_mag"].shift()
     peis["Z_phase_shifted"] = peis["Z_phase"].shift()
-
     peis["Impedanz1"] = peis[["Z_mag_shifted", "Z_phase_shifted", "Z_phase"]].apply(
         lambda imp_args: impedanzkorr(*imp_args), axis=1)
 
     return peis
 
 
+def calculate_cycle_data(peis: pd.DataFrame) -> pd.DataFrame:
+    peis_cycle = pd.DataFrame(peis[pd.notnull(peis["Impedanz1"])].iloc[:, :]["Impedanz1"])
+    peis_cycle["cycle"] = range(1, len(peis_cycle) + 1)
+    return peis_cycle
+
+
 class MprFilesHandler(LoggingFileSystemEventHandler):
+    def __init__(self):
+        self.psql_string = 'postgresql://{user}:{password}@postgres:5432/{db}'.format(user=POSTGRES_USER,
+                                                                                      password=POSTGRES_PASSWORD,
+                                                                                      db=POSTGRES_DB)
+        super(MprFilesHandler, self).__init__()
+
+    @staticmethod
+    def read_mpt(fpath: str) -> pd.DataFrame:
+        file_path, file_name = os.path.split(fpath)
+        return extract_mpt(file_path + "/", file_name)
+
+    @staticmethod
+    def find_file(files_list, postfix):
+        found_files = [f for f in files_list if f.endswith(postfix)]
+        if len(found_files) == 0:
+            raise Exception("No file with postfix %s found" % postfix)
+        elif len(found_files) > 1:
+            raise Exception("Too many files with postfix %s found" % postfix)
+        else:
+            logging.info("Found file for postfix %s" % postfix)
+            return found_files[0]
+
     def on_created(self, event):
         if event.is_directory:
+            # logging directory creation and awaiting for all files to be copied
             logging.info("Directory %s created" % event.src_path)
             time.sleep(5)
+
+            # listing files to find all data sources
             directory_files = [os.path.join(event.src_path, f) for f in os.listdir(event.src_path)]
+            all_mpt_files = [f for f in directory_files if f.endswith(".mpt")]
 
-            peis4_c01_files = [f for f in directory_files if f.endswith("09_PEIS_C01.mpt")]
-            cp_c01_files = [f for f in directory_files if f.endswith("08_CP_C01.mpt")]
+            logging.info("Presented .mpt files are:")
 
-            logging.info("PEIS file path: %s" % peis4_c01_files[0])
-            logging.info("CP data file path: %s" % cp_c01_files[0])
+            for mpt_file in all_mpt_files:
+                logging.info("\t %s" % mpt_file)
 
-            peis4_c01_df = prepare_peis_file(peis4_c01_files[0])
-            cp_c01_df = extract_mpt_wrapper(cp_c01_files[0])
+            peis09_path = MprFilesHandler.find_file(all_mpt_files, "09_PEIS_C01.mpt")
+            cp08_path = MprFilesHandler.find_file(all_mpt_files, "08_CP_C01.mpt")
+            peis05_path = MprFilesHandler.find_file(all_mpt_files, "05_PEIS_C01.mpt")
+            cv06_path = MprFilesHandler.find_file(all_mpt_files, "06_CV_C01.mpt")
 
-            peis4_value = pd.DataFrame(peis4_c01_df[pd.notnull(peis4_c01_df["Impedanz1"])].iloc[:, :]["Impedanz1"])
-            peis4_value["cycle"] = range(1, len(peis4_value) + 1)
+            peis09_df = MprFilesHandler.read_mpt(peis09_path)
+            cp08_df = MprFilesHandler.read_mpt(cp08_path)
+            peis05_df = MprFilesHandler.read_mpt(peis05_path)
+            cv06_df = MprFilesHandler.read_mpt(cv06_path)
 
-            cp_c01_df["times_diff"] = cp_c01_df["times"].diff(1).fillna(0).astype(int)
-            cp_c01_df["diff_indicator"] = cp_c01_df["times_diff"].map(lambda diff: 0 if diff <= 1 else 1)
-            cp_c01_df["times_grouping"] = cp_c01_df["diff_indicator"].cumsum() + 1
+            peis09_df = prepare_peis_data(peis09_df)
+            peis05_df = prepare_peis_data(peis05_df)
 
-            merged_df = cp_c01_df.merge(peis4_value, left_on="times_grouping", right_on="cycle")
+            peis09_cycle_data = calculate_cycle_data(peis09_df)
+            peis05_cycle_data = calculate_cycle_data(peis05_df)
+
+            peis05_impedanz_value = peis05_cycle_data.iloc[0, 0]
+            cv06_df["corr_voltage_vs_RHE"] = cv06_df["Ewe/V"] - ((cv06_df["I_avg"] / 1000) * peis05_impedanz_value)
+            surface_circle = 0.126
+            cv06_df["current_density"] = cv06_df["I_avg"] / surface_circle
+
+            min_current_density = cv06_df["current_density"].min()
+            cv06_df["current_density_verschoben"] = cv06_df["current_density"] - min_current_density
+            cv06_df["corr_voltage_vs_RHE_shifted"] = cv06_df["corr_voltage_vs_RHE"].shift()
+            cv06_df["current_density_verschoben_shifted"] = cv06_df["current_density_verschoben"].shift()
+            cv06_df["current_density_cumulative"] = (cv06_df["corr_voltage_vs_RHE"] - cv06_df[
+                "corr_voltage_vs_RHE_shifted"]) * (cv06_df["current_density_verschoben"] + cv06_df[
+                "current_density_verschoben_shifted"]) / 2
+
+            cv06_df_area = cv06_df.groupby("cycle_number")["current_density_cumulative"].sum().to_frame()
+            cycle_speed = 100
+            cv06_df_area["C"] = cv06_df_area["current_density_cumulative"] / cycle_speed * surface_circle
+            cv06_df_area = cv06_df_area.reset_index()
+
+            cp08_df["times_diff"] = cp08_df["times"].diff(1).fillna(0).astype(int)
+            cp08_df["diff_indicator"] = cp08_df["times_diff"].map(lambda diff: 0 if diff <= 1 else 1)
+            cp08_df["times_grouping"] = cp08_df["diff_indicator"].cumsum() + 1
+
+            merged_df = cp08_df.merge(peis09_cycle_data, left_on="times_grouping", right_on="cycle")
             merged_df["spanung"] = merged_df["Ewe/V"] - (merged_df["I/mA"] / 1000 * merged_df["Impedanz1"])
             merged_df["clean_times"] = (merged_df["times"] - merged_df["times"].min()).astype(int)
 
             merged_df.columns = [c.lower() for c in merged_df.columns]
 
-            result_df = merged_df[["impedanz1", "cycle", "clean_times", "spanung"]]
-            result_df["load_dttm"] = pd.datetime.now()
-            result_df["probe_index"] = event.src_path.split("/")[-1]
-            psql_string = 'postgresql://{user}:{password}@postgres:5432/{db}'.format(user=POSTGRES_USER,
-                                                                                     password=POSTGRES_PASSWORD,
-                                                                                     db=POSTGRES_DB)
-            engine = create_engine(psql_string)
+            result_df_cp = merged_df[["impedanz1", "cycle", "clean_times", "spanung"]]
+            result_df_cp["load_dttm"] = pd.datetime.now()
+            result_df_cp["probe_index"] = event.src_path.split("/")[-1]
+            engine = create_engine(self.psql_string)
             logging.info("Writing into database")
-            result_df.to_sql('f_experiment', engine, if_exists="replace", chunksize=50000, index=False)
+            result_df_cv = cv06_df[["Ewe/V", "I_avg","cycle_number", "corr_voltage_vs_RHE","current_density"]]
+            result_df_cp.to_sql('cp_experiment', engine, if_exists="replace", chunksize=50000, index=False)
+            result_df_cv.to_sql('cv_experiment', engine, if_exists="replace", chunksize=50000, index=False)
+            cv06_df_area.to_sql('cv_area_experiment', engine, if_exists="replace", chunksize=50000, index=False)
             logging.info("new experiment data written to database")
+
+
+
 
 
 if __name__ == '__main__':
